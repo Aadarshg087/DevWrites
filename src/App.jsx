4078fde3a332ea006d00a753da4ceede3dd13711
aadarsh <aadarshg087@gmail.com>
2025-02-28 00:28:14 +0530

    Added Basic Redux Functions
    Login
    Logout
    States

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import { useDispatch } from "react-redux";
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import authService from "./appwrite/auth.appwrite.js";
+import { login, logout } from "./store/authSlice.js";
+import { Footer, Header } from "./components/index.js";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>A blogging website</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex felx-wrap content-between bg-gray-400">
+      <div className="w-full block ">
+        <Header />
+        <main>{/* Outlet */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
